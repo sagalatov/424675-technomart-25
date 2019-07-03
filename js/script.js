a8c6fe702057d659ac57f01cefb9e80a7ee6d462
sagalatov sergey <sagalatov@mail.ru>
2019-07-03 18:22:46 +0300

    fixed problems with js file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var link = document.querySelector(".write-us");
 var popup = document.querySelector(".modal-feedback");
-var close = popup.querySelector(".modal-close");
-var form = popup.querySelector("form");
-var login = popup.querySelector("[name=user-name]");
-var email = popup.querySelector("[name=email]");
+var close = document.querySelector(".modal-close");
+var form = document.querySelector("form");
+var login = document.querySelector("[name=user-name]");
+var email = document.querySelector("[name=email]");
 var isStorageSupport = true;
 var storage = "";
 
@@ -13,7 +13,7 @@ try {
     isStorageSupport = false;
 }
 
-link.addEventListener("click", function(evt) {
+link && link.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
 
@@ -28,10 +28,10 @@ link.addEventListener("click", function(evt) {
 
 });
 
-close.addEventListener("click", function(evt) {
+close && close.addEventListener("click", function(evt) {
     evt.preventDefault();
-    popup.classList.remove("modal-show");
-    popup.classList.remove("modal-error");
+    popup && popup.classList.remove("modal-show");
+    popup && popup.classList.remove("modal-error");
 })
 
 form.addEventListener("submit", function(evt) {
@@ -51,39 +51,39 @@ form.addEventListener("submit", function(evt) {
 window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
         evt.preventDefault();
-        if (popup.classList.contains("modal-show")) {
-            popup.classList.remove("modal-show");
-            popup.classList.remove("modal-error");
+        if (popup && popup.classList.contains("modal-show")) {
+            popup && popup.classList.remove("modal-show");
+            popup && popup.classList.remove("modal-error");
         }
     }
 });
 
 var mapLink = document.querySelector(".contacts-button-map");
 var mapPopup = document.querySelector(".modal-map");
-var mapClose = mapPopup.querySelector(".modal-close");
+var mapClose = document.querySelector(".modal-map .modal-close");
 
-mapLink.addEventListener("click", function(evt) {
+mapLink && mapLink.addEventListener("click", function(evt) {
     evt.preventDefault();
     mapPopup.classList.add("modal-show");
 });
 
-mapClose.addEventListener("click", function(evt) {
+mapClose && mapClose.addEventListener("click", function(evt) {
     evt.preventDefault();
-    mapPopup.classList.remove("modal-show");
+    mapPopup && mapPopup.classList.remove("modal-show");
 });
 
 window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
         evt.preventDefault();
-        if (mapPopup.classList.contains("modal-show")) {
-            mapPopup.classList.remove("modal-show");
+        if (mapPopup && mapPopup.classList.contains("modal-show")) {
+            mapPopup && mapPopup.classList.remove("modal-show");
         }
     }
 });
 
 var buyLink = document.querySelectorAll(".buy");
 var cartPopup = document.querySelector(".modal-cart");
-var cartClose = document.querySelector(".modal-close");
+var cartClose = document.querySelector(".modal-cart .modal-close");
 var continueShopping = document.querySelector(".button-cart");
 
 for (var i = 0; i < buyLink.length; i++) {
@@ -93,22 +93,21 @@ for (var i = 0; i < buyLink.length; i++) {
     });
 }
 
-cartClose.addEventListener("click", function(evt) {
+cartClose && cartClose.addEventListener("click", function(evt) {
     evt.preventDefault();
-    cartPopup.classList.remove("modal-show");
+    cartPopup && cartPopup.classList.remove("modal-show");
 });
 
-continueShopping.addEventListener("click", function(evt) {
+continueShopping && continueShopping.addEventListener("click", function(evt) {
     evt.preventDefault();
-    cartPopup.classList.remove("modal-show");
+    cartPopup && cartPopup.classList.remove("modal-show");
 });
 
-window.addEventListener("keydown", function(evt) {
+window && window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
         evt.preventDefault();
-        if (cartPopup.classList.contains("modal-show")) {
+        if (cartPopup && cartPopup.classList.contains("modal-show")) {
             cartPopup.classList.remove("modal-show");
         }
     }
 });
-
